d0cd24c48b9310896e550fb2e50491d77283624e
sikander-afzaal <sikander.aaa@gmail.com>
2023-03-07 00:51:22 +0500

    update

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,6 +14,17 @@ export default function Home() {
   const line4 = useRef(null);
   const line5 = useRef(null);
   const secondSecPara = useRef();
+  //card section -----------
+  const card1 = useRef(null);
+  const card2 = useRef(null);
+  const card3 = useRef(null);
+  const card4 = useRef(null);
+  const card5 = useRef(null);
+  const card6 = useRef(null);
+  const thirdSection = useRef();
+  const cardWrapper = useRef(null);
+  const cardSection = useRef(null);
+  const scrollText = useRef(null);
 
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
@@ -97,6 +108,90 @@ export default function Home() {
         duration: 0.01,
         stagger: 0.01,
       });
+      //card section -----------------
+      const cardTime = gsap.timeline({
+        scrollTrigger: {
+          trigger: secondSec.current,
+          start: "center 25%",
+          end: "+=1000",
+          scrub: 3,
+        },
+      });
+      if (window.innerWidth > 800) {
+        cardTime
+          .to(thirdSection.current, { yPercent: -10 })
+          .to(
+            [
+              card1.current,
+              card2.current,
+              card3.current,
+              card4.current,
+              card5.current,
+              card6.current,
+            ],
+            {
+              y: 0,
+              opacity: 1,
+              stagger: 0.5,
+            },
+            "0"
+          )
+          .to(scrollText.current, { opacity: 1, delay: 0.3 }, "0");
+      } else {
+        cardTime
+          .to(
+            [
+              card1.current,
+              card2.current,
+              card3.current,
+              card4.current,
+              card5.current,
+              card6.current,
+            ],
+            {
+              y: 0,
+              opacity: 1,
+              stagger: 0.5,
+            },
+            "0"
+          )
+          .to(scrollText.current, { opacity: 1, delay: 0.3 }, "0");
+      }
+      ////////////////////////////////////////////////////////
+      const pinned = gsap.timeline({
+        scrollTrigger: {
+          trigger: thirdSection.current,
+          start: "top top",
+          end: "+=4500",
+          pin: thirdSection.current,
+          scrub: 1,
+          pinSpacing: true,
+        },
+      });
+      const CARDS_ON_SCREEN =
+        window.innerWidth > 800
+          ? (window.innerWidth - 70) / 640 // number of cards on the screen desktop
+          : (window.innerWidth - 70) / 415; // number of cards on the screen mobile
+      const CARD_MOVE =
+        window.innerWidth > 800
+          ? (6 - CARDS_ON_SCREEN) * 620 // amount to be moved desktop
+          : (6 - CARDS_ON_SCREEN) * 400; // amount to be moved mobile
+      const windowWidth = window.innerWidth - 35;
+      const TEXT_MOVE = scrollText.current.offsetWidth - windowWidth;
+
+      if (TEXT_MOVE > 0) {
+        pinned
+          .to(
+            cardSection.current,
+            { x: -CARD_MOVE, duration: 4, delay: 0.1 },
+            "0"
+          )
+          .to(
+            scrollText.current,
+            { x: -TEXT_MOVE, duration: 3.5, delay: 0.1 },
+            "0"
+          );
+      }
     }, [
       line1.current,
       line2.current,
@@ -192,7 +287,41 @@ export default function Home() {
               <span>Lorem </span>
             </p>
           </div>
-          <div className={styles.hero}></div>
+          <div ref={thirdSection} className={styles.thirdWrapper}>
+            <div ref={cardWrapper} className={styles.thirdSection}>
+              <div ref={cardSection} className={styles.cardDiv}>
+                <div
+                  ref={card1}
+                  className={`${styles.card} ${styles.card1}`}
+                ></div>
+                <div
+                  ref={card2}
+                  className={`${styles.card} ${styles.card2}`}
+                ></div>
+                <div
+                  ref={card3}
+                  className={`${styles.card} ${styles.card3}`}
+                ></div>
+                <div
+                  ref={card4}
+                  className={`${styles.card} ${styles.card4}`}
+                ></div>
+                <div
+                  ref={card5}
+                  className={`${styles.card} ${styles.card5}`}
+                ></div>
+                <div
+                  ref={card6}
+                  className={`${styles.card} ${styles.card6}`}
+                ></div>
+              </div>
+              <p ref={scrollText}>
+                CREATIVE, INNOVATIVE, IMAGINATIVE, POSITIVE, COLLECTIVE,
+                PASSIONATELY SEEKING MORE PASSIVE COMMUNITY INCOME BUILDING
+                OPPORTUNITIES for all
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </>
